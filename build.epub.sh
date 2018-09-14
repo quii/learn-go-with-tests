911e4e412744821932dbe042b6bd2f194c2124a5
Chris James <qui666@gmail.com>
2018-09-14 16:00:47 +0100

    script to build epub

---
+++ build.epub.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+docker run -v `pwd`:/source jagregory/pandoc -o learn-go-with-tests.epub --toc-depth=1 title.txt \
+    gb-readme.md \
+    hello-world.md \
+    integers.md \
+    arrays-and-slices.md \
+    structs-methods-and-interfaces.md \
+    pointers-and-errors.md \
+    maps.md \
+    dependency-injection.md \
+    mocking.md \
+    concurrency.md \
+    select.md \
+    reflection.md \
+    http-server.md \
+    json.md \
+    io.md \
+    command-line.md \
+    time.md \
