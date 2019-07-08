3e24ce021b62e8b745186317fe0bc83c4730007a
Chris James <qui666@gmail.com>
2019-07-08 14:55:27 +0100

    refactored the conversion from roman and separated the test code from the prod code

--- roman-numerals/v10/numeral_test.go
+++ roman-numerals/v10/numeral_test.go
@@ -2,7 +2,6 @@ package v1
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 )
 
@@ -63,77 +62,4 @@ func TestConvertingToArabic(t *testing.T) {
 			}
 		})
 	}
-}
-
-func ConvertToArabic(roman string) int {
-	total := 0
-
-	for i := 0; i < len(roman); i++ {
-		symbol := roman[i]
-
-		if couldBeSubtractive(i, symbol, roman) {
-			if value := romanNumerals.ValueOf(symbol, roman[i+1]); value != 0 {
-				total += value
-				i++ // move past this character too for the next loop
-			} else {
-				total += romanNumerals.ValueOf(symbol)
-			}
-		} else {
-			total += romanNumerals.ValueOf(symbol)
-		}
-	}
-	return total
-}
-
-func couldBeSubtractive(index int, currentSymbol uint8, roman string) bool {
-	isSubtractiveSymbol := currentSymbol == 'I' || currentSymbol == 'X' || currentSymbol == 'C'
-	return index+1 < len(roman) && isSubtractiveSymbol
-}
-
-type RomanNumeral struct {
-	Value  int
-	Symbol string
-}
-
-type RomanNumerals []RomanNumeral
-
-func (r RomanNumerals) ValueOf(symbols ...byte) int {
-	symbol := string(symbols)
-	for _, s := range r {
-		if s.Symbol == symbol {
-			return s.Value
-		}
-	}
-
-	return 0
-}
-
-var romanNumerals = RomanNumerals{
-	{1000, "M"},
-	{900, "CM"},
-	{500, "D"},
-	{400, "CD"},
-	{100, "C"},
-	{90, "XC"},
-	{50, "L"},
-	{40, "XL"},
-	{10, "X"},
-	{9, "IX"},
-	{5, "V"},
-	{4, "IV"},
-	{1, "I"},
-}
-
-func ConvertToRoman(arabic int) string {
-
-	var result strings.Builder
-
-	for _, numeral := range romanNumerals {
-		for arabic >= numeral.Value {
-			result.WriteString(numeral.Symbol)
-			arabic -= numeral.Value
-		}
-	}
-
-	return result.String()
 }
