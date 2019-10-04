202e89c17732d2469f9717323e78994f340c8d1e
Matt Hartenbower <matt.hartenbower@gmail.com>
2019-10-04 11:17:00 -0500

    Add mutex to avoid concurrent map read/write error (#255)
    Fixes #238

--- http-server/v5/InMemoryPlayerStore.go
+++ http-server/v5/InMemoryPlayerStore.go
@@ -1,21 +1,32 @@
 package main
 
+import "sync"
+
 // NewInMemoryPlayerStore initialises an empty player store
 func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{map[string]int{}}
+	return &InMemoryPlayerStore{
+		map[string]int{},
+		sync.RWMutex{},
+	}
 }
 
 // InMemoryPlayerStore collects data about players in memory
 type InMemoryPlayerStore struct {
 	store map[string]int
+	// A mutex is used to synchronize read/write access to the map
+	lock sync.RWMutex
 }
 
 // RecordWin will record a player's win
 func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.lock.RLock()
+	defer i.lock.RUnlock()
 	i.store[name]++
 }
 
 // GetPlayerScore retrieves scores for a given player
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	i.lock.Lock()
+	defer i.lock.Unlock()
 	return i.store[name]
 }
