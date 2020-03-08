d84a9bd65fceb46b318cda32920efd2232f3b2de
Chris James <qui666@gmail.com>
2020-03-08 11:52:06 +0000

    Context reader (#321)
    * context reader chap
    
    * some corrections, less waffle
    
    * Update README.md
    
    * making naming consistent with chapter heading
    
    * add link to context reader to gitbook

---
+++ q-and-a/context-aware-reader/context_aware_reader.go
@@ -0,0 +1,26 @@
+package cancelreader
+
+import (
+	"context"
+	"io"
+)
+
+// NewCancellableReader will stop reading to rdr if ctx is cancelled
+func NewCancellableReader(ctx context.Context, rdr io.Reader) io.Reader {
+	return &readerCtx{
+		ctx:      ctx,
+		delegate: rdr,
+	}
+}
+
+type readerCtx struct {
+	ctx      context.Context
+	delegate io.Reader
+}
+
+func (r *readerCtx) Read(p []byte) (n int, err error) {
+	if err := r.ctx.Err(); err != nil {
+		return 0, err
+	}
+	return r.delegate.Read(p)
+}
---
+++ q-and-a/context-aware-reader/context_aware_reader_test.go
@@ -0,0 +1,62 @@
+package cancelreader
+
+import (
+	"context"
+	"strings"
+	"testing"
+)
+
+func TestContextAwareReader(t *testing.T) {
+	t.Run("behaves like a normal reader", func(t *testing.T) {
+		rdr := NewCancellableReader(context.Background(), strings.NewReader("123456"))
+		got := make([]byte, 3)
+		_, err := rdr.Read(got)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assertBufferHas(t, got, "123")
+
+		_, err = rdr.Read(got)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assertBufferHas(t, got, "456")
+	})
+
+	t.Run("stops reading when cancelled", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		rdr := NewCancellableReader(ctx, strings.NewReader("123456"))
+		got := make([]byte, 3)
+		_, err := rdr.Read(got)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assertBufferHas(t, got, "123")
+
+		cancel()
+
+		n, err := rdr.Read(got)
+
+		if err == nil {
+			t.Error("expected an error after cancellation but didnt get one")
+		}
+
+		if n > 0 {
+			t.Errorf("expected 0 bytes to be read after cancellation but %d were read", n)
+		}
+	})
+}
+
+func assertBufferHas(t *testing.T, buf []byte, want string) {
+	t.Helper()
+	got := string(buf)
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
