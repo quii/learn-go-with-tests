d20d13ffa4471c52afab7e1888bb678e2b99a1e1
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-05-05 14:51:39 +0100

    working go code with for synthax

---
+++ repeat_test.go
@@ -0,0 +1,10 @@
+package iteration
+
+
+func Repeat(character string) string {
+    var repeated string
+    for i := 0; i < 5; i++ {
+        repeated = repeated + character
+    }
+    return repeated
+}
