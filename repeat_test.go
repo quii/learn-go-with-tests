76f67fd39632adb0a06b3854d91c9ece2b5d7cb6
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-05-05 14:53:08 +0100

    added the +=

--- repeat_test.go
+++ repeat_test.go
@@ -1,10 +1,12 @@
 package iteration
 
 
+const repeatCount = 5
+
 func Repeat(character string) string {
     var repeated string
-    for i := 0; i < 5; i++ {
-        repeated = repeated + character
+    for i := 0; i < repeatCount; i++ {
+        repeated += character
     }
     return repeated
 }
