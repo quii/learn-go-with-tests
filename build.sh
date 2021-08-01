340302fbf6cbb593bdd9df6b4f25c353106b4fc8
N. Fontenot <wobbol@users.noreply.github.com>
2021-08-01 10:04:45 -0500

    Create github action (#484)

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -e
 
 go get github.com/gorilla/websocket #todo vendor this or learn about the module stuff!
+go get -u golang.org/x/lint/golint
 go get -u github.com/client9/misspell/cmd/misspell
 
 ls *.md | xargs misspell -error
