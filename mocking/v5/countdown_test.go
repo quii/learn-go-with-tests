e90adb53fffcfb4545c057f85b2d435b6d1978d1
Mark DL <78699952+markdl-dev@users.noreply.github.com>
2021-08-20 19:23:26 +0800

    -renamed CountdownOperationsSpy (#488)

--- mocking/v5/countdown_test.go
+++ mocking/v5/countdown_test.go
@@ -11,7 +11,7 @@ func TestCountdown(t *testing.T) {
 
 	t.Run("prints 3 to Go!", func(t *testing.T) {
 		buffer := &bytes.Buffer{}
-		Countdown(buffer, &CountdownOperationsSpy{})
+		Countdown(buffer, &SpyCountdownOperations{})
 
 		got := buffer.String()
 		want := `3
@@ -25,7 +25,7 @@ Go!`
 	})
 
 	t.Run("sleep before every print", func(t *testing.T) {
-		spySleepPrinter := &CountdownOperationsSpy{}
+		spySleepPrinter := &SpyCountdownOperations{}
 		Countdown(spySleepPrinter, spySleepPrinter)
 
 		want := []string{
@@ -57,15 +57,15 @@ func TestConfigurableSleeper(t *testing.T) {
 	}
 }
 
-type CountdownOperationsSpy struct {
+type SpyCountdownOperations struct {
 	Calls []string
 }
 
-func (s *CountdownOperationsSpy) Sleep() {
+func (s *SpyCountdownOperations) Sleep() {
 	s.Calls = append(s.Calls, sleep)
 }
 
-func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
 	s.Calls = append(s.Calls, write)
 	return
 }
