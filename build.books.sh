c7c0dedb888bf75ac5452102335b60fb3d042169
Bharath Kumar Uppala <uppala.bharath@gmail.com>
2021-11-17 00:54:58 +0530

    Made changes to add cover page before the toc

--- build.books.sh
+++ build.books.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-docker run --rm -v `pwd`:/data uppalabharath/pandoc-latex-cjk:latest --from=gfm+rebase_relative_paths --to=latex -o learn-go-with-tests.pdf -H meta.tex --pdf-engine=xelatex --variable urlcolor=blue --toc --toc-depth=1 \
-    pdf-cover.md \
+docker run --rm -v `pwd`:/data uppalabharath/pandoc-latex-cjk:latest --from=gfm+rebase_relative_paths -o learn-go-with-tests.pdf \
+    -H meta.tex --pdf-engine=xelatex --variable urlcolor=blue --toc --toc-depth=1 \
+    -B pdf-cover.tex \
     gb-readme.md \
     why.md \
     hello-world.md \
