539ae1c791d172153cc5cf5210d947669f723758
Saurabh Mishra <saurabh.mish@icloud.com>
2023-05-15 18:00:33 -0700

    corrected message

--- http-server/v1/server.go
+++ http-server/v1/server.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-// PlayerServer currently returns Hello, world given _any_ request.
+// PlayerServer currently returns "20" given _any_ request.
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "20")
 }
