0aa2b680fa2da2cc8d74d4dff1bac037791ae8de
Paulus Esterhazy <pesterhazy@gmail.com>
2024-06-07 15:46:30 +0200

    Remove reference to term "accumulator" (#773)

--- arrays/v8/collection_fun.go
+++ arrays/v8/collection_fun.go
@@ -9,10 +9,10 @@ func Find[A any](items []A, predicate func(A) bool) (value A, found bool) {
 	return
 }
 
-func Reduce[A, B any](collection []A, accumulator func(B, A) B, initialValue B) B {
+func Reduce[A, B any](collection []A, f func(B, A) B, initialValue B) B {
 	var result = initialValue
 	for _, x := range collection {
-		result = accumulator(result, x)
+		result = f(result, x)
 	}
 	return result
 }
