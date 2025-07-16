c5a3d26fe06ee5b4ea1acbf42d2a12ec52e1e215
Frédéric Delsert <2428794+fdelsert@users.noreply.github.com>
2025-07-16 10:21:44 +0200

    fix: change SpyTime receiver function name from Sleep to SetDurationSlept (#753) (#866)

--- mocking/v6/countdown_test.go
+++ mocking/v6/countdown_test.go
@@ -48,7 +48,7 @@ func TestConfigurableSleeper(t *testing.T) {
 	sleepTime := 5 * time.Second
 
 	spyTime := &SpyTime{}
-	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.SetDurationSlept}
 	sleeper.Sleep()
 
 	if spyTime.durationSlept != sleepTime {
@@ -76,6 +76,6 @@ type SpyTime struct {
 	durationSlept time.Duration
 }
 
-func (s *SpyTime) Sleep(duration time.Duration) {
+func (s *SpyTime) SetDurationSlept(duration time.Duration) {
 	s.durationSlept = duration
 }
